9d2c8d45275b09fc952c20f07d966ec45adf1483
Lewis <ljoncb@gmail.com>
2022-07-31 22:06:12 +0100

    word search input bug fix

--- components/word-search.tsx
+++ components/word-search.tsx
@@ -1,10 +1,11 @@
 import { useTranslation } from 'next-i18next'
 import { useRouter } from 'next/router'
 import { Word } from 'osdpjs'
-import { ChangeEvent, FormEvent, useContext, useState } from 'react'
+import { ChangeEvent, ChangeEventHandler, FormEvent, useContext, useState } from 'react'
 import { LanguageContext } from '../contexts/language'
+import { Language } from '../models/language'
 import { SupportedWordTypes } from '../models/words'
-import { SupportedLanguages } from '../services/languages'
+import { LanguageFromCode, SupportedLanguages } from '../services/languages'
 import styles from '../styles/word-search.module.css'
 
 export interface WordSearchProps {
@@ -20,28 +21,26 @@ export const WordSearch: React.FC<WordSearchProps> = ({
     initialWordToSearch,
     result,
 }) => {
-    const { language, changeLanguage } = useContext(LanguageContext)
+    const initialLanguage = useContext(LanguageContext).language
+    const [ language, setLanguage] = useState<Language>(initialLanguage)
     const [wordType, setWordType] = useState<string>(initialWordType)
     const [selectSearch, setSelectSearch] =
         useState<string>(initialSearchSelect)
     const [wordToSearch, setWordToSearch] =
         useState<string>(initialWordToSearch)
+    const [ startedNewSearch, setStartedNewSearch ] = useState<boolean>(false)
     const { t } = useTranslation()
     const router = useRouter()
 
-    if (result && wordToSearch !== result.normalForm) {
+    if (result && wordToSearch !== result.normalForm && !startedNewSearch) {
         setWordToSearch(result.normalForm)
     }
 
     const handleLanguageSelect = (e: ChangeEvent<HTMLSelectElement>) => {
-        const newLangCode = e.target.value
-        const language = SupportedLanguages.find(
-            (lang) => lang.code === newLangCode
-        )
-        if (!language) {
-            throw new Error('non-support language selected')
-        }
-        changeLanguage(language)
+       const newLanguage = LanguageFromCode(e.target.value)
+       if(newLanguage) {
+           setLanguage(newLanguage)
+       }
     }
 
     const LanguageSelect: React.FC = () => {
@@ -99,6 +98,7 @@ export const WordSearch: React.FC<WordSearchProps> = ({
 
     const handleSubmit = (e: FormEvent) => {
         e.preventDefault()
+        console.log('word to search', wordToSearch)
         const { locale } = router
         const url = `/${locale}/${language.code}/${wordToSearch}/`
         console.log('locale', locale)
@@ -115,6 +115,13 @@ export const WordSearch: React.FC<WordSearchProps> = ({
         )
     }
 
+    const updateWordToSearch = (e: ChangeEvent<HTMLInputElement>) => {
+        const newVal = e.target.value
+        setWordToSearch(newVal)
+        setStartedNewSearch(true)
+        console.log('set newVal', newVal)
+    }
+
     return (
         <div className={styles['word-search-container']}>
             <form className={styles['word-search']} onSubmit={handleSubmit}>
@@ -126,7 +133,7 @@ export const WordSearch: React.FC<WordSearchProps> = ({
                     type="text"
                     placeholder={t('word-search.search-box')}
                     value={wordToSearch}
-                    onChange={(e) => setWordToSearch(e.target.value)}
+                    onChange={updateWordToSearch}
                     required
                 />
                 <button
